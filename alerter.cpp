4c587fc08b5f1d1e10648ff5fdca9762d4d00b53
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-06-21 10:30:05 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -4,17 +4,22 @@
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
-    std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    return 200;
+    int alertReturn = 200;
+    if (celcius > 200) {
+      std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+      // Return 200 for ok
+      // Return 500 for not-ok
+      // stub always succeeds and returns 200
+      alertReturn = 500;
+    }
+    return alertReturn;
 }
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
+        std::cout << "returnCode is : " <<returnCode<<std::endl;
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
@@ -26,7 +31,17 @@ void alertInCelcius(float farenheit) {
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
-    std::cout << alertFailureCount << " alerts failed.\n";
+    alertInCelcius(450.6);
+    alertInCelcius(420.6);
+    if (alertFailureCount == 3) {
+      std::cout <<" Alerts count is success, count is: "<<alertFailureCount<<std::endl;
+    }
+    else {
+      std::cout <<" Alerts count is failed, count is: "<<alertFailureCount<<std::endl;
+    }
+    assert(alertFailureCount == 3);
     std::cout << "All is well (maybe!)\n";
     return 0;
 }
+
+
