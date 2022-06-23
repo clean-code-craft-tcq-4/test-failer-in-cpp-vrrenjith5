5fbea0b0ed5917d7ca7670f300d4740aae892350
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-06-23 11:43:30 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,18 +1,26 @@
 #include <iostream>
 #include <assert.h>
 
-extern "C" {
-#include "stub.h"
-}
-
-extern int networkAlertStub(float celcius);
+#define ALERT_NOT_REQUIRED      (200)
+#define ALERT_REQUIRED          (500)
 
 int alertFailureCount = 0;
 
+int networkAlertStub(float celcius) {
+    int alertReturn = ALERT_NOT_REQUIRED;
+    if (celcius > ALERT_NOT_REQUIRED) {
+      std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
+      // Return 200 for ok
+      // Return 500 for not-ok
+      // stub always succeeds and returns 200
+      alertReturn = ALERT_REQUIRED;
+    }
+    return alertReturn;
+}
+
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
-    //int returnCode = networkAlertStub(celcius);
-    int returnCode  = 0;
+    int returnCode = networkAlertStub(celcius);
     if (returnCode != ALERT_NOT_REQUIRED) {
         std::cout << "returnCode is : " <<returnCode<<std::endl;
         // non-ok response is not an error! Issues happen in life!
