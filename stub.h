f8c79495a32c1d758690a5e68dc8d6fad3b4f8de
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-06-28 20:56:41 +0530

    Add files via upload

---
+++ stub.h
@@ -0,0 +1,13 @@
+#ifndef __STUB_H_
+#define __STUB_H_
+
+#define ALERT_NOT_REQUIRED      (200)
+#define ALERT_REQUIRED          (500)
+
+int networkAlertStub(float celcius);
+
+/*export API's*/
+
+extern int networkAlertStub(float celcius);
+
+#endif /*__STUB_H_*/
