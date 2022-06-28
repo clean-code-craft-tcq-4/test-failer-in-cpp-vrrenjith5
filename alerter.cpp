a9a8f01d7ab9481748d11e8c4d8c39075298f133
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-06-28 21:02:23 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -2,20 +2,19 @@
 #include <assert.h>
 #include "stub.h"
 
-
 int alertFailureCount = 0;
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != ALERT_NOT_REQUIRED) {
-        std::cout << "returnCode is : " <<returnCode<<std::endl;
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 1;
     }
+    std::cout << "returnCode is : " <<returnCode<<std::endl;
 }
 
 int main() {
