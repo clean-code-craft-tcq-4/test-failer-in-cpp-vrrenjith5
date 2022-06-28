3f7c87d791329d20e0210aa5370e8cb401e872a2
Renjith Varvila Reghu <vrrenjith5@gmail.com>
2022-06-28 20:57:27 +0530

    Update alerter.cpp

--- alerter.cpp
+++ alerter.cpp
@@ -1,22 +1,9 @@
 #include <iostream>
 #include <assert.h>
+#include "stub.h"
 
-#define ALERT_NOT_REQUIRED      (200)
-#define ALERT_REQUIRED          (500)
 
 int alertFailureCount = 0;
-
-int networkAlertStub(float celcius) {
-    int alertReturn = ALERT_NOT_REQUIRED;
-    if (celcius > ALERT_NOT_REQUIRED) {
-      std::cout << "ALERT: Temperature is " << celcius << " celcius.\n";
-      // Return 200 for ok
-      // Return 500 for not-ok
-      // stub always succeeds and returns 200
-      alertReturn = ALERT_REQUIRED;
-    }
-    return alertReturn;
-}
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
